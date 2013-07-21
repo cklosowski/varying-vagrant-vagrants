0935ee18041e9558d9cee1a81d3777ae85213d8c
U-JOMAX\cklosowski <cklosowski@LT126454-CKLOS.jomax.paholdings.com>
2013-07-20 21:14:13 -0700

    Using wpcli to install debug plugins

--- provision/provision-post.sh
+++ provision/provision-post.sh
@@ -1,88 +1,59 @@
-# Checkout, install and configure debug bar trunk
+cd /srv/www/wordpress-trunk/
+# Add Plugins
 if [ ! -d /srv/www/wordpress-trunk/wp-content/plugins/debug-bar ]
 then
-	printf "Checking out Debug Bar trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar/trunk/ /srv/www/wordpress-trunk/wp-content/plugins/debug-bar
+	wp plugin install debug-bar --activate
 else
-	printf "Updating Debug Bar trunk...\n"
-	cd /srv/www/wordpress-trunk/wp-content/plugins/debug-bar
-	svn up
+	wp plugin update debug-bar 
 fi
 
-# Checkout, install and configure debug bar trunk
-if [ ! -d /srv/www/wordpress-default/wp-content/plugins/debug-bar ]
-then
-	printf "Checking out Debug Bar trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar/trunk/ /srv/www/wordpress-default/wp-content/plugins/debug-bar
-else
-	printf "Updating Debug Bar trunk...\n"
-	cd /srv/www/wordpress-default/wp-content/plugins/debug-bar
-	svn up
-fi
-
-# Checkout, install and configure debug bar console trunk
 if [ ! -d /srv/www/wordpress-trunk/wp-content/plugins/debug-bar-console ]
 then
-	printf "Checking out Debug Bar Console trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar/trunk/ /srv/www/wordpress-trunk/wp-content/plugins/debug-bar
+	wp plugin install debug-bar-console --activate
 else
-	printf "Updating Debug Bar Console trunk...\n"
-	cd /srv/www/wordpress-trunk/wp-content/plugins/debug-bar
-	svn up
+	wp plugin update debug-bar-console
 fi
 
-# Checkout, install and configure debug bar console trunk
-if [ ! -d /srv/www/wordpress-default/wp-content/plugins/debug-bar-console ]
-then
-	printf "Checking out Debug Bar Console trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar-console/trunk/ /srv/www/wordpress-default/wp-content/plugins/debug-bar-console
-else	
-	printf "Updating Debug Bar Console trunk...\n"
-	cd /srv/www/wordpress-default/wp-content/plugins/debug-bar-console
-	svn up
-fi
-
-
-# Checkout, install and configure user switching trunk
 if [ ! -d /srv/www/wordpress-trunk/wp-content/plugins/debug-bar-cron ]
 then
-	printf "Checking out user switching trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar/trunk/ /srv/www/wordpress-trunk/wp-content/plugins/debug-bar-cron
+	wp plugin install debug-bar-cron --activate
 else
-	printf "Updating user switching trunk...\n"
-	cd /srv/www/wordpress-trunk/wp-content/plugins/debug-bar
-	svn up
+	wp plugin update debug-bar-cron
 fi
 
-# Checkout, install and configure user switching trunk
-if [ ! -d /srv/www/wordpress-default/wp-content/plugins/debug-bar-cron ]
-then
-	printf "Checking out user switching trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar-cron/trunk/ /srv/www/wordpress-default/wp-content/plugins/debug-bar-cron
-else
-	printf "Updating user switching trunk...\n"
-	cd /srv/www/wordpress-default/wp-content/plugins/debug-bar-cron
-	svn up
-fi
-
-# Checkout, install and configure user switching trunk
 if [ ! -d /srv/www/wordpress-trunk/wp-content/plugins/user-switching ]
 then
-	printf "Checking out user switching trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/debug-bar/trunk/ /srv/www/wordpress-trunk/wp-content/plugins/user-switching
+	wp plugin install user-switching --activate
 else
-	printf "Updating user switching trunk...\n"
-	cd /srv/www/wordpress-trunk/wp-content/plugins/debug-bar
-	svn up
+	wp plugin update user-switching
 fi
 
-# Checkout, install and configure user switching trunk
+cd /srv/www/wordpress-default/
+# Add Plugins
+if [ ! -d /srv/www/wordpress-default/wp-content/plugins/debug-bar ]
+then
+	wp plugin install debug-bar --activate
+else
+	wp plugin update debug-bar 
+fi
+
+if [ ! -d /srv/www/wordpress-default/wp-content/plugins/debug-bar-console ]
+then
+	wp plugin install debug-bar-console --activate
+else
+	wp plugin update debug-bar-console
+fi
+
+if [ ! -d /srv/www/wordpress-default/wp-content/plugins/debug-bar-cron ]
+then
+	wp plugin install debug-bar-cron --activate
+else
+	wp plugin update debug-bar-cron
+fi
+
 if [ ! -d /srv/www/wordpress-default/wp-content/plugins/user-switching ]
 then
-	printf "Checking out user switching trunk....\n"
-	svn checkout http://plugins.svn.wordpress.org/user-switching/trunk/ /srv/www/wordpress-default/wp-content/plugins/user-switching
+	wp plugin install user-switching --activate
 else
-	printf "Updating user switching trunk...\n"
-	cd /srv/www/wordpress-default/wp-content/plugins/user-switching
-	svn up
+	wp plugin update user-switching
 fi
